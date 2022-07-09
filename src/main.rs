8723994352a191873b25a856e213809a2f6f1825
jermsam <ssali@jitpomi.com>
2022-07-09 13:42:40 -0600

    String deep copy

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,20 @@ fn main() {
 }
 
 // string literals, easy copy
+/*
+If a type implements the Copy trait, variables that use it do not move,
+ but rather are trivially copied, making them still valid after assignment to another variable.
+*/
 
-fn string_literal_copy() {
+fn string_literal_copy() { // x and y variables here implement the Copy trait
     // double quote str literals, single quote chars, put underscore for unused vars
     let x = "Sam"; // x enters scope .... RAII bind the value 5 to x
     let y = x; // y enters into scope ... make a copy of the value in x and bind it to y
-    println!("both {} and {} in scope", x, y);
+    println!("both x = {} and y = {} in scope", x, y);
     println!("This is indeed what is happening, \
     because integers are simple values with a known, \
     fixed size, and these two 5 values are pushed onto the stack.");
-} // drop is called, x and y go out of scope and both copies of 5 are popped off the stack
+} // drop is called, x and y go out of scope and both copies of content are popped off the stack
 
 // String , ownership managed copy
 
@@ -33,4 +37,10 @@ fn sting_shallow_copy() {
     println!("We say x moved to y to mean that at y = x; \
     the pointer, length and capacity for x were copied into y but not its content")
 } // drop is called, x and y go out of scope but memory is only cleared for y
+
+fn string_deep_copy() {
+    let x = String::from("Sam"); // x comes into scope
+    let y = x.clone(); // y enters into scope ... make a copy of the value in x and bind it to y
+    println!("Expensive deep clone x = {} and y = {}.", x,y)
+} // drop is called, x and y go out of scope and both copies of content are popped off the stack
 
