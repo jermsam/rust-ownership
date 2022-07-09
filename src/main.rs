19900487c9a6e8b84880ceea431494e993b6c18b
jermsam <ssali@jitpomi.com>
2022-07-09 11:47:55 -0600

    string literal copy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    string_literal_copy();
 }
 
 // string literals, easy copy
 
 fn string_literal_copy() {
     // double quote str literals, single quote chars
-    let x = "Sam";
+    let _x = "Sam";
 }
